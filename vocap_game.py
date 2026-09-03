edfb7792aa3f1057d3658841d7e2084c9603be42
411-19-kornkamon <54553@yupparaj.ac.th>
2026-09-03 13:38:34 +0700

    Update vocap_game.py

--- vocap_game.py
+++ vocap_game.py
@@ -47,7 +47,7 @@ def show_result_dialog(ans1, ans2):
         score += 1
     else:
         st.error(f"❌ ข้อ 3: ยังไม่ถูกต้อง (คุณตอบ '{u_ans3}')")
-     if u_ans4 == "hand":
+    if u_ans4 == "hand":
         st.success("✅ ข้อ 4: ถูกต้อง")
         score += 1
     else:
