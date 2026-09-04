f4f7d5f95f0412287c7b3f7c608a33ef894d5ca1
411-19-kornkamon <54553@yupparaj.ac.th>
2026-09-04 12:29:08 +0700

    Update vocap_game.py

--- vocap_game.py
+++ vocap_game.py
@@ -53,14 +53,14 @@ def show_result_dialog(ans1, ans2, ans3, ans4):
         st.error(f"❌ ข้อ 2: ยังไม่ถูกต้อง (คุณตอบ '{u_ans2}')")
 
         # ตรวจข้อ 3
-    if u_ans3 == "car":
+    if u_ans3 == "pencil":
         st.success("✅ ข้อ 3: ถูกต้อง")
         score += 1
     else:
         st.error(f"❌ ข้อ 3: ยังไม่ถูกต้อง (คุณตอบ '{u_ans3}')")
 
         # ตรวจข้อ 4
-    if u_ans4 == "gift":
+    if u_ans4 == "phone":
         st.success("✅ ข้อ 4: ถูกต้อง")
         score += 1
     else:
