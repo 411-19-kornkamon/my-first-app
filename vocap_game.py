edd9c7b0f4110ac5b648a852ce869446c66add82
411-19-kornkamon <54553@yupparaj.ac.th>
2026-09-04 12:28:07 +0700

    Update vocap_game.py

--- vocap_game.py
+++ vocap_game.py
@@ -103,7 +103,7 @@ ans2 = st.text_input(
     value=st.session_state.ans2_val,
 )
 ans3 = st.text_input(
-    "ข้อ 3: Can I borrow your `p _ _ c_l`✏️",
+    "ข้อ 3: Can I borrow your `p _ _ c_l`. ✏️",
     value=st.session_state.ans3_val,
 )
 ans4 = st.text_input(
